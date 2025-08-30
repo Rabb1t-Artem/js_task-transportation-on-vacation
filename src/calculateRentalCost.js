e71f0f5b4eae6c6b7a3058f4ac6bba8afc9d417d
Artem Shatylo <alukard32244@gmail.com>
2025-08-30 18:01:54 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const longTerm = 7;
-  const shirtTerm = 3;
-  const longTermDisc = 50;
-  const shirtTermDisc = 20;
-  const result = days * 40;
+  const LONG_TERM_THRESHOLD = 7;
+  const SHORT_TERM_THRESHOLD = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const RESULT = days * 40;
 
-  if (days >= shirtTerm && days < 7) {
-    return result - shirtTermDisc;
-  } else if (days >= longTerm) {
-    return result - longTermDisc;
+  if (days >= SHORT_TERM_THRESHOLD && days < 7) {
+    return RESULT - SHORT_TERM_DISCOUNT;
   }
 
-  return result;
+  if (days >= LONG_TERM_THRESHOLD) {
+    return RESULT - LONG_TERM_DISCOUNT;
+  }
+
+  return RESULT;
 }
 
 module.exports = calculateRentalCost;
